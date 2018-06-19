32b0698e3d0784d12f97ce75421d027ba75380ff
john.gt82@gmail.com <john.gt82@gmail.com>
2018-06-19 16:28:40 +1000

    from work

--- Docx Email To Word Press/Properties/Settings.Designer.cs
+++ Docx Email To Word Press/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace DocxEmailToWordPress.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.5.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.7.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -200,6 +200,15 @@ namespace DocxEmailToWordPress.Properties {
         public string multiSchoolTitle {
             get {
                 return ((string)(this["multiSchoolTitle"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("60")]
+        public int postTimeOut {
+            get {
+                return ((int)(this["postTimeOut"]));
             }
         }
     }
--- Docx Email To Word Press/WordPressApi.cs
+++ Docx Email To Word Press/WordPressApi.cs
@@ -14,16 +14,19 @@ namespace DocxEmailToWordPress
 
         private String wpSiteUsername = Properties.Settings.Default.wpSiteUsername;
         private String wpSitePassword = Properties.Settings.Default.wpSitePassword;
+        private int postTimeOut = Properties.Settings.Default.postTimeOut; 
 
-
+        
 
         public IRestResponse PostData(String contents, String title)
         {
-            
 
+            var timeOut = postTimeOut;
             var client = new RestClient(wpApiUrl);
             client.Authenticator = new HttpBasicAuthenticator(wpSiteUsername, wpSitePassword);
-           
+            client.Timeout = 60;
+            var apiTimeOut = client.Timeout;
+            
 
             var request = new RestRequest(Method.POST);
             request.AddHeader("Content-Type", "application/json");
@@ -38,10 +41,34 @@ namespace DocxEmailToWordPress
            
             IRestResponse response = client.Execute(request);
 
+            if (response.IsSuccessful)
+            {
+                return response;
 
+            }else
+            {
+                do
+                {
+                   System.Threading.Thread.Sleep(1000);
+
+                    Console.Write("Count is: " + timeOut);
+
+                    if(apiTimeOut > 0)
+                    {
+                        response = client.Execute(request);
+                    }
+
+                    timeOut--;
+
+                } while (!response.IsSuccessful && timeOut > 0);
+
+
+
+            }
 
 
             return response;
+            
         }
 
 
