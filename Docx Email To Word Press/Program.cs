84ca1e85551f6352ae2d6e5c2b43976f398dc2ce
john.gt82@gmail.com <john.gt82@gmail.com>
2020-01-02 17:11:00 +1100

    office

--- Docx Email To Word Press/Program.cs
+++ Docx Email To Word Press/Program.cs
@@ -8,10 +8,28 @@ namespace DocxEmailToWordPress
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        /// 
+       // <summary>
+       // The main entry point for the application.
+       // </summary>
+
+       // Docx Email to WordPress website, this program will download emails with D.E.T ST positions
+       // and scrap the document for the relevant position information and then repost it to our company website via the
+       // word press API
+       // Copy-left (Copy left) 2019  John Thompson
+
+        // This program is free software: you can redistribute it and/or modify
+        // it under the terms of the GNU General Public License as published by
+        // the Free Software Foundation, either version 3 of the License, or
+        // (at your option) any later version.
+
+        // This program is distributed in the hope that it will be useful,
+        // but WITHOUT ANY WARRANTY; without even the implied warranty of
+        // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        // GNU General Public License for more details.
+
+        // You should have received a copy of the GNU General Public License
+        // along with this program.  If not, see <https://www.gnu.org/licenses/>
+
 
         // log4net class log name
         private static readonly log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
